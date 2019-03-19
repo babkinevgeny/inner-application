fe25e09d1aeb7f95937d3e6af3e1ecc52cd93693
Evgeny Babkin <babkinevgeny@gmail.com>
2019-03-19 17:29:40 +0300

    Added styles for plug. Set routing and controller for plug

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
+
+const routes = require('./routes/routes');
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!');
-})
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views/global'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
 
 app.listen(3000);
